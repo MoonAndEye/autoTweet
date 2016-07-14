c9aff53028756588d527fd2a5748181f27147720
Moonandeye <MoonAndEye@users.noreply.github.com>
2016-07-14 11:17:30 +0900

    pandas Dataframe was done
    toDo
    a> 做出三種pattern
    b> 做出圖

--- twRandom.py
+++ twRandom.py
@@ -13,7 +13,7 @@ import random as rd
 time_axis = []
 
 """
-三個小時半的交易, 就是270 min
+四個小時半的交易, 就是270 min
 """
 
 
@@ -21,37 +21,43 @@ startTime = "09:01"
 startPoint = 8832.42
 singleRange = startPoint * 0.1
 singleRange = int(singleRange)
+up_limit = rd.randint(int(startPoint),int(startPoint + singleRange))
+low_limit = rd.randint(int(startPoint - singleRange), int(startPoint))
+
 """
 singleRange 是單邊範圍,到時候可以下一個random(singleRange, singleRange * -1)
 """
 
 timeIndex = pd.date_range(startTime, periods = 270, freq="min")
 
-predict = pd.DataFrame(np.random.randn(len(timeIndex)), columns=["twStock"], index = timeIndex)
+#print(len(timeIndex))
 
-print(len(timeIndex))
+minutes_point = []
 
-min_index = []
-min_index.append(startPoint)
+for each_min in range(len(timeIndex)):
+    each_point = rd.randint(low_limit, up_limit)
+    minutes_point.append(each_point)
 
+predict = pd.DataFrame(minutes_point, columns=["twStock"], index = timeIndex)
 
+"""
 for each_min in range(len(timeIndex) -1):
     init = min_index[each_min]
     nextPoint = init + rd.randint(int(-singleRange), singleRange)
     min_index.append(nextPoint)    
-
 """
-這樣子失敗了,因為不斷的相加,不斷的減少,很容易就會超過上下限
-怎麼辦?
-我要怎麼模擬,台股有上下限的
-所以,改成 while loop ?
-只要長度和 timeIndex 沒有 match, 就不斷的產生一個數字
-而只有這個數字在限制的
-
-
-換個想法
-
+"""
 一開始丟出一個上限,和一個下限
 
 然後 random 的東西就直接設定在這區間,就不會超過啦
+
+先畫圖,之後再改點數,這個點數很不正常,可能要加入一常態分佈的狀況
+目前先設定上下限, 當天的上下限就只會在這個地方跑
+
+toDo List
+1> 做出三種 pattern
+    a> 開高走高
+    b> 開低走低
+    c> 上下震盪
+
 """
