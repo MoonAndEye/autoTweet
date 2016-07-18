53bd0767694e71666cca4f577855bd101cb50757
Moonandeye <MoonAndEye@users.noreply.github.com>
2016-07-19 08:59:58 +0900

    把 def 的註解加上去

--- periodTime.py
+++ periodTime.py
@@ -1,9 +1,33 @@
-import datetime
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 16:08:04 2016
 
+"""
+import pandas as pd
+import numpy as np
+#import time
+import datetime
+import matplotlib as plt
+import random as rd
+
+
+d0 = datetime.date.today()
+start = datetime.datetime.today()
+start = start.replace(hour=9,minute=0)
+start = start + datetime.timedelta(minutes = 30)
+#print(start)
+d0 = d0 - datetime.timedelta(minutes = 10)   #如果以後要換日期，就用這個
+#test = [start]
+a = datetime.time(9,0)
+b = datetime.time(13,10)
+
+print(a,b)
 
 def periodTime(start=list, end=list, step=int):
     """
     You must import datetime
+    Start and end is a list with two elements.
+    Ex. you can use [9,0] as 09:00, [13,30] as 13:00
     """ 
     output= []
     for hours in range(start[0], end[0] + 1):
