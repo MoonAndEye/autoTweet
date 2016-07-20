f9c6271cd35b5a1ade234174562af4ce3b16f0b2
Moonandeye <MoonAndEye@users.noreply.github.com>
2016-07-20 18:16:37 +0900

    做出台股加權指數

--- TAIEX.py
+++ TAIEX.py
@@ -2,4 +2,21 @@ from googlefinance import getQuotes
 
 import json
 
-print (json.dumps(getQuotes('TPE:TAIEX'), indent=2))
+test = json.dumps(getQuotes('TPE:TAIEX'))
+
+a = getQuotes('TPE:TAIEX')
+
+empty = []
+
+for each in a[0]:
+    empty.append(each)
+
+#a = []
+
+result = a[0][empty[5]]
+new = float(result.replace(",",""))
+
+new = int(new)
+print(result)
+print(type(result))
+print((float(result.replace(",",""))))
